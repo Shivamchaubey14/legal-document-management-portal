cd53e4972ed4a5140761bd722fa7299124feac07
Shivam <shivamc36@gmail.com>
2025-06-21 17:51:27 +0530

    removed the email credential

--- documents_management/settings.py
+++ documents_management/settings.py
@@ -135,12 +135,4 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # e.g., 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shivamc36@gmail.com'
-EMAIL_HOST_PASSWORD = 'vytl mlnw fwms fdcg'
-DEFAULT_FROM_EMAIL = 'shivamc36@gmail.com'
